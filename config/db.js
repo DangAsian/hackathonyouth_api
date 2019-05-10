0823c24d7a0f40428acc8e0fc7b821a171641665
Daniel Ang <dan.justin.ang@gmail.com>
2019-05-09 23:47:46 -0400

    added authorization middleware and created youth model

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI')
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db)
-    console.log('MongoDB connected...')
-  } catch(err) {
-    console.log(err.message)
-    process.exit(1)
+    await mongoose.connect(
+      db,
+      { useNewUrlParser: true, useCreateIndex: true }
+    );
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const connectDB = require('./config/db')
 
 const app = express();
 
-//Connect DB
+// Connect DB
 connectDB();
+
+// Init Middleware
+// Already part of Express now, don't need to install body parser
+app.use(express.json({extended: false}))
 
 app.get('/', (req, res) => {
   res.send(`Api Running on port ${PORT}`)
 })
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
 
 const PORT = process.env.PORT || 3000
 
